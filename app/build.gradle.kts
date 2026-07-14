fdb71443e0fb1c2662dbaf430bde6ea3f371c0e0
zihao liu <3750217581@qq.com>
2026-07-14 16:35:52 +0800

    chore: bump version to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.gift.tolife"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.0.0"
+        versionCode = 15
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
